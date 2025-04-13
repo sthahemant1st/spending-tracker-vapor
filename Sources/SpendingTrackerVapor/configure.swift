a017f461e9a6fad371b6dd333e596ac07fff979e
Hemant Shrestha <sthahemant1st@gmail.com>
2025-04-13 17:30:44 +0200

    practice Command

--- Sources/SpendingTrackerVapor/configure.swift
+++ Sources/SpendingTrackerVapor/configure.swift
@@ -7,4 +7,52 @@ public func configure(_ app: Application) async throws {
 
     // register routes
     try routes(app)
+    app.asyncCommands.use(HelloCommand(), as: "hello")
+    app.asyncCommands.use(Cowsay(), as: "cowsay")
+}
+
+struct HelloCommand: AsyncCommand {
+    struct Signature: CommandSignature {}
+
+    var help: String {
+        "Says hello"
+    }
+
+    func run(using context: CommandContext, signature: Signature) async throws {
+        let name = context.console.ask("What is your \("name", color: .blue)?")
+        context.console.print("Hello, \(name) 👋")
+    }
+}
+
+struct Cowsay: AsyncCommand {
+    struct Signature: CommandSignature {
+        @Argument(name: "message")
+        var message: String
+
+        @Option(name: "eyes", short: "e")
+        var eyes: String?
+
+        @Option(name: "tongue", short: "t")
+        var tongue: String?
+    }
+
+    var help: String {
+        "Generates ASCII picture of a cow with a message."
+    }
+
+    func run(using context: CommandContext, signature: Signature) async throws {
+        let eyes = signature.eyes ?? "oo"
+        let tongue = signature.tongue ?? "  "
+        let cow = #"""
+          < $M >
+                  \   ^__^
+                   \  ($E)\_______
+                      (__)\       )\/\
+                       $T ||----w |
+                          ||     ||
+        """#.replacingOccurrences(of: "$M", with: signature.message)
+            .replacingOccurrences(of: "$E", with: eyes)
+            .replacingOccurrences(of: "$T", with: tongue)
+        context.console.print(cow)
+    }
 }
